bbeb8fdbaa51c110bcc279943f5cd863d2f6bd26
Michael-Fares <mjfares@gmail.com>
2021-08-10 14:12:32 -0500

    db initialization.sql file edited and finalized

--- initialization.sql
+++ initialization.sql
@@ -1,39 +1,47 @@
-CREATE TABLE `Decisions` (
-  `decision_id` int,
-  `user_id` int,
-  `decision_text` varchar(100),
-  PRIMARY KEY (`decision_id`)
+DROP TABLE IF EXISTS Options_Criteria_Rankings, Criteria, Options, Decisions, Users;
+
+CREATE TABLE Users (
+  user_id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(50),
+  last_name VARCHAR(50),
+  email VARCHAR(50),
+  PRIMARY KEY (user_id)
 );
 
-CREATE TABLE `Users` (
-  `user_id` int,
-  `first_name` varchar(50),
-  `last_name` varchar(50),
-  `email` varchar(50),
-  PRIMARY KEY (`user_id`)
+
+CREATE TABLE Decisions (
+  decision_id INT NOT NULL AUTO_INCREMENT,
+  user_id INT,
+  decision_text VARCHAR(100),
+  PRIMARY KEY (decision_id),
+  FOREIGN KEY (user_id) REFERENCES Users(user_id)
+
 );
 
-CREATE TABLE `options_criteria_rankings` (
-  `ranking_id` int,
-  `option_id` int,
-  `criterion_id` int,
-  `option_rank_on_criterion` tinyint,
-  PRIMARY KEY (`ranking_id`),
-  FOREIGN KEY (`option_id`) REFERENCES `Options`(`option_id`)
+
+CREATE TABLE Options (
+  option_id INT NOT NULL AUTO_INCREMENT,
+  decision_id INT,
+  option_text VARCHAR(100),
+  PRIMARY KEY (option_id),
+  FOREIGN KEY (decision_id) REFERENCES Decisions(decision_id)
 );
 
-CREATE TABLE `Options` (
-  `option_id` int,
-  `decision_id` int,
-  `option_text` varchar(100),
-  PRIMARY KEY (`option_id`)
+CREATE TABLE Criteria (
+  criterion_id INT NOT NULL AUTO_INCREMENT,
+  decision_id INT,
+  ctiterion_text VARCHAR(100),
+  criterion_importance TINYINT,
+  PRIMARY KEY (criterion_id),
+  FOREIGN KEY (decision_id) REFERENCES Decisions(decision_id)
 );
 
-CREATE TABLE `Criteria` (
-  `criterion_id` int,
-  `decision_ id` int,
-  `ctiterion_text` varchar(100),
-  `criterion_importance` tinyint,
-  PRIMARY KEY (`criterion_id`)
+CREATE TABLE Options_Criteria_Rankings (
+  ranking_id INT NOT NULL AUTO_INCREMENT,
+  option_id INT,
+  criterion_id INT,
+  option_rank_on_criterion TINYINT,
+  PRIMARY KEY (ranking_id),
+  FOREIGN KEY (option_id) REFERENCES Options(option_id),
+  FOREIGN KEY (criterion_id) REFERENCES Criteria(criterion_id)
 );
-
